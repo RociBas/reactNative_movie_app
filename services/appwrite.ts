438ebb33684a7f7d1287e42af2b372fc763b7b2d
RocioBas <rocio.bas.f.@gmail.com>
2025-07-21 13:17:46 +0200

    appwrite setup

---
+++ services/appwrite.ts
@@ -0,0 +1,46 @@
+import { Client, Databases, ID, Query } from "react-native-appwrite";
+
+//track searches made by a user
+
+const DATABASE_ID = process.env.EXPO_PUBLIC_APPWRITE_DATABASE_ID!;
+const COLLECTION_ID = process.env.EXPO_PUBLIC_APPWRITE_COLLECTION_ID!;
+
+const client = new Client()
+  .setEndpoint("https://fra.cloud.appwrite.io/v1")
+  .setProject(process.env.EXPO_PUBLIC_APPWRITE_PROJECT_ID!);
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (query: string, movie: Movie) => {
+  try {
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal("searchTerm", query),
+    ]);
+    // check if a record of that search has alredy benn stored
+    if (result.documents.length > 0) {
+      const existingMovie = result.documents[0];
+
+      await database.updateDocument(
+        DATABASE_ID,
+        COLLECTION_ID,
+        existingMovie.$id,
+        {
+          count: existingMovie.count + 1,
+        }
+      );
+    } else {
+      await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        title: movie.title,
+        searchTerm: query,
+        movie_id: movie.id,
+        count: 1,
+        poster_url: `https://image.tmdb.org/t/p/w500${movie.poster_path}`,
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+  // if a document if found increment the searchCount field
+  //if not document is found create new document in Appwrite database -> 1
+};
